a9667315881dec7c23053e9dec917368ba8895fd
Sudalairajan. A <sudalairajan.a@devopstrioglobal.com>
2026-05-08 15:25:17 +0530

    Devopstrio test version 2.7

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
     <>
       {/* HERO */}
       <Hero />
-      {/* PARTNER ECOSYSTEM */}
+      {/* PARTNER_ECOSYSTEM */}
       <PartnerEcosystem />
 
       {/* WhatWeDo */}
